3a1b3e33a34d72baf5283c38ce7c4542171ffa52
dustinquintanilla <portillo0121@gmail.com>
2024-05-06 11:41:23 -0400

    now uses movie posters instead of meme images

--- src/guestMainMenu.java
+++ src/guestMainMenu.java
@@ -16,7 +16,7 @@ public class guestMainMenu {
     public guestMainMenu() {
         JFrame frame = new JFrame("Guest Main Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 500);
+        frame.setSize(700, 600);
         frame.add(panel1);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
@@ -24,10 +24,10 @@ public class guestMainMenu {
 
 
         // Load the image
-        ImageIcon movieIcon1 = new ImageIcon(getClass().getResource("/images/titanic.jpeg"));
+        ImageIcon movieIcon1 = new ImageIcon(getClass().getResource("/images/titanic.jpg"));
         // Scale the image
         Image image1 = movieIcon1.getImage();
-        Image scaledImage1 = image1.getScaledInstance(200, 150, Image.SCALE_SMOOTH);
+        Image scaledImage1 = image1.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
         // Create a new ImageIcon from the scaled image
         ImageIcon scaledIcon1 = new ImageIcon(scaledImage1);
         // Set the icon of button1 to the scaled image
@@ -37,11 +37,31 @@ public class guestMainMenu {
         ImageIcon movieIcon2 = new ImageIcon(getClass().getResource("/images/mario_movie.jpeg"));
         // Scale the image
         Image image2 = movieIcon2.getImage();
-        Image scaledImage2 = image2.getScaledInstance(200, 150, Image.SCALE_SMOOTH);
+        Image scaledImage2 = image2.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
         // Create a new ImageIcon from the scaled image
         ImageIcon scaledIcon2 = new ImageIcon(scaledImage2);
         // Set the icon of button1 to the scaled image
         label2.setIcon(scaledIcon2);
+
+        // Load the image
+        ImageIcon movieIcon3 = new ImageIcon(getClass().getResource("/images/kung_fu_panda.jpeg"));
+        // Scale the image
+        Image image3 = movieIcon3.getImage();
+        Image scaledImage3 = image3.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
+        // Create a new ImageIcon from the scaled image
+        ImageIcon scaledIcon3 = new ImageIcon(scaledImage3);
+        // Set the icon of button1 to the scaled image
+        label3.setIcon(scaledIcon3);
+
+        // Load the image
+        ImageIcon movieIcon4 = new ImageIcon(getClass().getResource("/images/cinderella.jpg"));
+        // Scale the image
+        Image image4 = movieIcon4.getImage();
+        Image scaledImage4 = image4.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
+        // Create a new ImageIcon from the scaled image
+        ImageIcon scaledIcon4 = new ImageIcon(scaledImage4);
+        // Set the icon of button1 to the scaled image
+        label4.setIcon(scaledIcon4);
 
     }
 
